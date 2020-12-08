c0f402dbaa7be99b8ad94225bef13b4c7f00f166
MrAnyx <1ptitigre@gmail.com>
2020-12-08 21:04:00 +0100

    Feat: Euhh, i think i f$cked up 🤔

---
+++ MyEyesCry.php
@@ -0,0 +1,71 @@
+<?php
+
+const 🔔 = 1593236583;
+
+const ➕ = [
+	[2, 1, 0],
+	[1, 0, 1],
+	[0, 1, 1]
+];
+
+$words = ["cadeau", "pourri", "cadeau", "revendre", "écharpe", "transformer", "smartbox", "pyrénéens", "goldeneye64", "cadeau"];
+
+function filterUsingTresMovaiCode😂(string $word)
+{
+	$😨 = [];
+	$hash = hash("adler32", $word);
+	$letters = str_split($hash);
+	foreach ($letters as $letter) {
+		$😨[] = array_merge([0], str_split(decbin(ord($letter))), [0]);
+	}
+	while(count($😨) > 2) {
+		$😴 = [];
+		for ($👽=1; $👽 < count($😨)-1; $👽++) { 
+			for ($👻=1; $👻 < count($😨)-1; $👻++) { 
+				$😴[$👽-1][$👻-1] = convolutionCalcul😲($😨, $👽, $👻);
+			}
+		}
+		$😨 = $😴;
+	}	
+	expandMatrixLoL($😨);
+
+	$😵 = 0;
+
+	foreach ($😨 as $row) {
+		$😵+=array_sum($row);
+	}
+
+	$🥶 = [
+		implode("", array_slice(str_split(strval($😵)), 0, 2)),
+		implode("", array_slice(str_split(strval($😵)), 2, 3))
+	];
+
+	$🐸 = new DateTime();
+	$🐸->setTimestamp(🔔);
+
+	return implode("-", $🥶) === $🐸->format("d-m");
+}
+
+function convolutionCalcul😲($😨, $👽, $👻) {
+	return $😨[$👽-1][$👻-1]*➕[0][0]+
+	$😨[$👽-1][$👻]*➕[0][1]+
+	$😨[$👽-1][$👻+1]*➕[0][2]+
+	$😨[$👽][$👻-1]*➕[1][0]+
+	$😨[$👽][$👻]*➕[1][1]+
+	$😨[$👽][$👻+1]*➕[1][2]+
+	$😨[$👽+1][$👻-1]*➕[2][0]+
+	$😨[$👽+1][$👻]*➕[2][1]+
+	$😨[$👽+1][$👻+1]*➕[2][2];
+}
+
+function expandMatrixLoL(&$😨) {
+	$😨 = [
+		[$😨[0][0], $😨[0][0]+$😨[0][1], $😨[0][1]],
+		[$😨[0][0]+$😨[1][0], abs($😨[0][0]*$😨[1][1]-$😨[1][0]*$😨[0][1]), $😨[0][1]+$😨[1][1]],
+		[$😨[1][0], $😨[1][0]+$😨[1][1], $😨[1][1]]
+	];
+}
+
+echo array_sum(array_map(function ($word) {
+	return intval(filterUsingTresMovaiCode😂($word));
+}, $words));
